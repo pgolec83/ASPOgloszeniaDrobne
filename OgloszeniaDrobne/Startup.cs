26c995d669a50d91e922d92b954fb5a179e263d6
Paweł Golec <pgolec83@gmail.com>
2019-01-15 21:12:52 +0100

    Dodaj pliki projektów.

---
+++ OgloszeniaDrobne/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OgloszeniaDrobne.Startup))]
+namespace OgloszeniaDrobne
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
